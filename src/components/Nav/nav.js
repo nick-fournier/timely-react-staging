130377d95b3538eb3a87955e149333d618850d45
unknown <adelkafouri50@gmail.com>
2021-06-19 19:12:02 +0200

    added new working nav for mobile

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -10,15 +10,24 @@ import { faStopwatch } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import {useHistory} from 'react-router-dom'
 
 export default function Nav({loadPayables, loadReceivables, setcurrentNavItem, currentNavItem, ShowNewInvoice, setShowNewInvoice, setShowNewBusiness, isActive, setisActive, setisMobile}) {
     const [showNav, setshowNav] = useState(false)
     const History = useHistory()
+    const [showMobileNavList1, setshowMobileNavList1] = useState(false)
+    const [showMobileNavList2, setshowMobileNavList2] = useState(false)
 
     const black = {
         color: 'black',
         fontWeight: 'bold'
+    }
+    const showNav1 = {
+        transform: 'translate(0%,51%)'
+    }
+    const showNav2 = {
+        transform: 'translate(147.5%,51%)'
     }
 
     return (
@@ -127,6 +136,109 @@ export default function Nav({loadPayables, loadReceivables, setcurrentNavItem, c
                 </li>
 
             </ul>
+
+            <div className="MobileNavContainer">
+                <div className="MobileNavFirstRow">
+                    <span onClick={()=>{
+                        setshowMobileNavList1(!showMobileNavList1)
+                        setshowMobileNavList2(false)
+                    }} className="icon">
+                        <FontAwesomeIcon icon ={faPlus} />
+                    </span>
+                    <div className="MobileNavLogo">
+                        <span className="icon ">
+                            <FontAwesomeIcon icon={faStopwatch} />
+                        </span>
+                        <span className="text">
+                            timely
+                        </span>
+                    </div>
+                    <div className="MobileNavSettings">
+                        <span onClick={()=>{
+                            setshowMobileNavList2(!showMobileNavList2)
+                            setshowMobileNavList1(false)
+                            }} className="icon">
+                            <FontAwesomeIcon icon ={faCog} />
+                        </span>
+                    </div>
+                </div>
+                <div className="MobileNavSecondRow">
+                    <div className={currentNavItem === 2? 'ActiveNavItem1':'nav-item'} onClick={()=>{
+                        loadReceivables()
+                        setshowNav(false)
+                        setShowNewInvoice(false)
+                        setisActive(false)
+                        setisMobile(false)
+                    }}>
+                        <span className="icon">
+                            <FontAwesomeIcon icon={faMoneyCheckAlt} />
+                        </span>
+                        <span className="text">
+                            Receivables
+                        </span>
+                    </div>
+                    <div className={currentNavItem === 3?'ActiveNavItem2':"nav-item2"} onClick={()=>{
+                        loadPayables()
+                        setshowNav(false)
+                        setShowNewInvoice(false)
+                        setisActive(false)
+                        setisMobile(false)
+                    }}>
+                        <span className="icon">
+                            <FontAwesomeIcon icon={faMoneyCheckAlt} />
+                        </span>
+                        <span className="text">
+                            Payables
+                        </span>
+                    </div>
+                </div>
+
+                <div style={showMobileNavList1?showNav1:{}} className='MobileNavListLeft'>
+                    <div className="navItemMobile" onClick ={()=>{
+                        setShowNewBusiness(false)
+                        setShowNewInvoice(!ShowNewInvoice)
+                        {isActive?setisActive(false):setisActive(true)}
+                    }}>
+                        <span className="icon">
+                            <FontAwesomeIcon icon ={faPlus} />
+                        </span>
+                        <span className="text">
+                            New Invoice
+                        </span>
+                    </div>
+                    <div className="navItemMobile">
+                        <span className="icon">
+                            <FontAwesomeIcon icon ={faPlus} />
+                        </span>
+                        <span className="text">
+                            New Payment
+                        </span>
+                    </div>
+                </div>
+
+                <div style={showMobileNavList2?showNav2:{}} className='MobileNavListRight'>
+                    <div className="navItemMobile2">
+                        <span className="icon">
+                            <FontAwesomeIcon icon ={faQuestionCircle} />
+                        </span>
+                        <span className="text">
+                            Support
+                        </span>
+                    </div>
+                    <div className="navItemMobile2">
+                        <span className="icon">
+                            <FontAwesomeIcon icon ={faSignOutAlt} />
+                        </span>
+                        <span onClick={()=>{
+                            setisActive(false)
+                            localStorage.setItem('token', '')
+                            History.push('/login')
+                        }} className="text">
+                            Log out
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
