433fc8f3e08c8c6047fdd1de49efa475a8fd6863
Adel Kafouri <adelkafouri@Adels-MacBook-Pro.local>
2021-07-13 18:20:46 +0200

    added stripe onboarding

--- src/components/PaymentSettings/PaymentSettings.jsx
+++ src/components/PaymentSettings/PaymentSettings.jsx
@@ -2,12 +2,28 @@
 import './PaymentSettings.css'
 import {useForm} from 'react-hook-form'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useState, useEffect } from 'react';
 
 
 export default function PaymentSettings({ShowPaymentSettings, setShowNewInvoice, setShowPaymentSettings, CurrentItem}) {
 
+    const [RedirectURL, setRedirectURL] = useState('')
+    const [RedirectTrigger, setRedirectTrigger] = useState(false)
 
 
+    useEffect( async () => {
+        const httpResponse = await fetch('https://timely-invoicing-api.herokuapp.com/pay/onboard',{
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': `token ${localStorage.token}`,
+            })
+        })
+
+        const JsonResponse = await httpResponse.json()
+        console.log(JsonResponse.url)
+        setRedirectURL(JsonResponse.url)
+        
+    }, [])
 
     const {register, handleSubmit, setValue } = useForm();
 
@@ -21,23 +37,21 @@ export default function PaymentSettings({ShowPaymentSettings, setShowNewInvoice,
         transform: 'translateX(100%)'
     }
 
-    async function onSubmit(data){
-
-
-
-
-    }
-
     return (
             <div style={ShowPaymentSettings? ShowPaymentSettingsTab: HidePaymentSettings} className='PaymentSettingsContainer'>
-
-                <div className="SubmitAndDiscardContainer">
-                    <button className="SubmitPaymentButton">Connect Stripe</button>
+                <div className="PaymentSettingsForm">
+                    <div className="PaymentSettingsHeader">
+                        Connect your stripe account to timely in order to use our immediate payment solutions!
+                    </div>
+                    <div className="SubmitAndDiscardContainer">
+                    <a className='OnBoardingButton' href={RedirectURL}> Connect Stripe</a>
                     <button className="DiscardInvoiceButton" onClick={(e)=>{
                                 setShowPaymentSettings(false)
                                 e.preventDefault()
                             }}> Discard</button>
+                    </div>
                 </div>
+
             </div>
     )
 }
