08285d883e804bd61ee338a44160125cc70d396f
Adel Kafouri <adelkafouri@Adels-MacBook-Pro.local>
2021-07-19 10:22:27 +0200

    fixed memory leak bug on stripe onboarding

--- src/components/PaymentSettings/PaymentSettings.jsx
+++ src/components/PaymentSettings/PaymentSettings.jsx
@@ -3,27 +3,37 @@ import './PaymentSettings.css'
 import {useForm} from 'react-hook-form'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useState, useEffect } from 'react';
+import { setIntervalAsync, clearIntervalAsync } from 'set-interval-async/dynamic';
 
 
 export default function PaymentSettings({ShowPaymentSettings, setShowNewInvoice, setShowPaymentSettings, CurrentItem}) {
 
     const [RedirectURL, setRedirectURL] = useState('')
     const [RedirectTrigger, setRedirectTrigger] = useState(false)
-
+    const [Refresh, setRefresh] = useState(false)
+    const [Loading, setLoading] = useState(false)
+    
 
     useEffect( async () => {
-        const httpResponse = await fetch('https://timely-invoicing-api.herokuapp.com/pay/onboard',{
-            method: 'GET',
-            headers: new Headers({
-                'Authorization': `token ${localStorage.token}`,
-            })
-        })
-
-        const JsonResponse = await httpResponse.json()
-        console.log(JsonResponse.url)
-        setRedirectURL(JsonResponse.url)
-        
-    }, [])
+        setIntervalAsync(async () => {
+                setLoading(true)
+                const httpResponse = await fetch('https://timely-invoicing-api.herokuapp.com/pay/onboard',{
+                    method: 'GET',
+                    headers: new Headers({
+                        'Authorization': `token ${localStorage.token}`,
+                    })
+                })
+                const JsonResponse = await httpResponse.json()
+                console.log(JsonResponse.url)
+                setRedirectURL(JsonResponse.url)
+                
+                return function cleanup(){
+                    clearIntervalAsync()
+                    setLoading(false)
+                }
+        }, 300000);
+    }, [Loading])
+    
 
     const {register, handleSubmit, setValue } = useForm();
 
